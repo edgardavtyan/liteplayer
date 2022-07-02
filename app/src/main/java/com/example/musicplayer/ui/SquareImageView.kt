af4ab4a7c4d2c803702c0cb71e5907a1179499b0
Edhar <edgarda.1994@gmail.com>
2022-07-02 20:01:56 +0300

    Remove ConstraintLayout dependency

---
+++ app/src/main/java/com/example/musicplayer/ui/SquareImageView.kt
@@ -0,0 +1,12 @@
+package com.example.musicplayer.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ImageView
+
+class SquareImageView(context: Context, attrs: AttributeSet): ImageView(context, attrs) {
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        setMeasuredDimension(measuredWidth, measuredWidth)
+    }
+}
