226c3a98148ab60a5ec30a9a1f60392f19192e81
Edhar <edgarda.1994@gmail.com>
2022-12-30 23:28:08 +0200

    Refactor NowPlayingModel

--- app/src/main/java/com/example/musicplayer/ui/nowplaying/NowPlayingModel.kt
+++ app/src/main/java/com/example/musicplayer/ui/nowplaying/NowPlayingModel.kt
@@ -2,18 +2,13 @@ package com.example.musicplayer.ui.nowplaying
 
 import android.content.ComponentName
 import android.content.Context
-import android.content.Intent
-import android.content.ServiceConnection
 import android.graphics.Bitmap
 import android.os.IBinder
-import com.example.musicplayer.service.PlayerService
-import com.example.musicplayer.service.PlayerServiceBinder
+import com.example.musicplayer.PlayerServiceConnection
 import com.example.musicplayer.ui.CoverReader
 
-class NowPlayingModel(private val context: Context, private val coverReader: CoverReader)
-    : ServiceConnection {
-
-    private lateinit var service: PlayerService
+class NowPlayingModel(context: Context, private val coverReader: CoverReader)
+    : PlayerServiceConnection(context) {
 
     var coverArt: Bitmap? = null
 
@@ -24,17 +19,12 @@ class NowPlayingModel(private val context: Context, private val coverReader: Cov
     var onServiceConnectedListener: OnServiceConnectedListener? = null
     var onIsPlayingChangedListener: ((isPlaying: Boolean) -> Unit)? = null
 
-    fun bind() {
-        val intent = Intent(context, PlayerService::class.java)
-        context.bindService(intent, this, Context.BIND_AUTO_CREATE)
+    fun playPause() {
+        service.player.playPause()
     }
 
-    fun unbind() {
-        context.unbindService(this)
-    }
-
-    override fun onServiceConnected(name: ComponentName?, binder: IBinder) {
-        service = (binder as PlayerServiceBinder).service
+    override fun onServiceConnected(name: ComponentName?, binder: IBinder?) {
+        super.onServiceConnected(name, binder)
         service.player.addOnIsPlayingChangedListener { onIsPlayingChangedListener?.invoke(it) }
 
         if (service.player.track != null) {
@@ -42,12 +32,5 @@ class NowPlayingModel(private val context: Context, private val coverReader: Cov
         }
 
         onServiceConnectedListener?.onConnected()
-    }
-
-    override fun onServiceDisconnected(name: ComponentName?) {
-    }
-
-    fun playPause() {
-        service.player.playPause()
     }
 }
